9d9f0d0c67f5d9312c0494994fc95e783d7ba7aa
FranciscoRobles <j-fr_23@hotmail.com>
2015-04-27 22:51:51 -0500

    index

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 var app = angular.module('app', []);
+app.controller('personCtrl', function($scope) {
+    $scope.person = {
+        firstName: "John",
+        lastName: "Doe"
+    };
+    $scope.myVar = true;
+    $scope.toggle = function() {
+        $scope.myVar = !$scope.myVar;
+    };
+});
