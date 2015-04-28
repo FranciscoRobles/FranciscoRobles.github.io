66cc50aee440490dcb2fe2f6273ad3df1062ea7b
FranciscoRobles <j-fr_23@hotmail.com>
2015-04-28 00:10:29 -0500

    index

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = angular.module('myApp', []);
 app.controller('personCtrl', function($scope) {
     $scope.person = {
         firstName: "John",
-        lastName: "Doe"
+        lastName: "The following terms and conditions ("Terms and Conditions") govern your use of the website give http://franciscorobles.github.io/Links/Restaurantes.html(el "Web Site") and any of the contents available from or through this Web Site, including any content derived therefrom. Gluten Free has made available the Website. We may change the Terms and Conditions of Use at any time without notice, only posting the changes on the Website. TO USE THE WEB SITE, YOU ACCEPT AND AGREE TO THESE TERMS AND CONDITIONS WITH REGARD TO YOUR USE OF THE WEB SITE. If you do not agree to these Terms and Conditions, you may not have access to it or use the Website in any other way."
     };
     $scope.myVar = true;
     $scope.toggle = function() {
