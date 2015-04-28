0fd9348fd867c8d4b6b2cdedae885dc3abc45532
FranciscoRobles <j-fr_23@hotmail.com>
2015-04-28 00:22:43 -0500

    index

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 var app = angular.module('myApp', []);
 app.controller('personCtrl', function($scope) {
-    $scope.person = {
-        firstName: "John",
-        lastName: "."
-    };
     $scope.myVar = true;
     $scope.toggle = function() {
         $scope.myVar = !$scope.myVar;
